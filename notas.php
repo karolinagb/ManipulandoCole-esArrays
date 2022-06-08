dab5298a5c06a1d2d14f98afe2c556d9569be53d
x <karolina_bp@hotmail.com>
2022-06-08 10:39:20 -0300

    Entendendo sobre a função array_is_list

--- notas.php
+++ notas.php
@@ -30,3 +30,35 @@ krsort($notas);
 //uksort = da pra usar a ordenação por critério em ordem alfabetica tb
 
 var_dump($notas);
+
+//gettype() - recebe uma variável e retorna o tipo dela
+//Se eu quiser verificar se uma variável é array:
+
+if(gettype($notas) == 'array')
+{
+    echo "Sim, é um array\n";
+};
+
+//Posso usar também o is_array:
+
+if(is_array($notas) == 'array')
+{
+    echo 'Sim, é um array';
+};
+
+var_dump(array_is_list($notas));
+
+function funcao(array $valor){
+    //como saber se um array é numérico
+    //ou seja, começa com 0 e são crescentes 
+    //Para fazer um for nele
+    //Podemos usar a função array_is_list
+    //Retorna verdadeiro se esse array for numerico - todas as chaves sao numeros e começa com 0 e 
+    //vai crescente
+    //Retorna falso se esse array não for uma lista sequencial
+    array_is_list($valor);
+
+    for($i = 0; $i < count($valor); $i++){
+
+    }
+}
